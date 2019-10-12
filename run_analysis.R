218fb55e993f02dcd35ae8f8425bc58ce028353f
puck-akx <51957973+puck-akx@users.noreply.github.com>
2019-10-13 04:02:26 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,77 @@
+# FILE
+#   run_analysis.R
+
+# OVERVIEW
+#   Using data collected from the accelerometers from the Samsung Galaxy S 
+#   smartphone, work with the data and make a clean data set, outputting the
+#   resulting tidy data to a file named "tidy_data.txt".
+#   See README.md for details.
+
+rm(list=ls())
+library(dplyr)
+ 
+# download zip file containing data if it hasn't already been downloaded
+zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zipFile <- "UCI HAR Dataset.zip"
+
+if (!file.exists(zipFile)) {
+  download.file(zipUrl, zipFile, mode = "wb")
+}
+
+# unzip zip file containing data if data directory doesn't already exist
+dataPath <- "UCI HAR Dataset"
+if (!file.exists(dataPath)) {
+  unzip(zipFile)
+}
+
+
+# reading all data tables
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+
+# combining training and testing data with labels and subject information 
+x <- rbind(x_train, x_test)
+y <- rbind(y_train, y_test)
+subject <- rbind(subject_train, subject_test)
+merged_data <- cbind(subject, y, x)
+
+
+# extracting measurements on the mean and standard deviation for
+# each measurement
+tidy_data <- merged_data %>% select(subject, code, contains("mean"), contains("std"))
+
+
+# using descriptive activity namees to describe activities
+tidy_data <- tidy_data %>% mutate(code = activities[code,2])
+
+
+# labeling data set with descriptive variable names
+names(tidy_data) <- gsub("(\\.)", "", names(tidy_data))
+names(tidy_data)[1] = "Subject"
+names(tidy_data)[2] = "Activity"
+names(tidy_data)<-gsub("Acc", "Accelerometer", names(tidy_data))
+names(tidy_data)<-gsub("Gyro", "Gyroscope", names(tidy_data))
+names(tidy_data)<-gsub("BodyBody", "Body", names(tidy_data))
+names(tidy_data)<-gsub("Mag", "Magnitude", names(tidy_data))
+names(tidy_data)<-gsub("freq", "Frequency", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("^t", "Time", names(tidy_data))
+names(tidy_data)<-gsub("^f", "Frequency", names(tidy_data))
+names(tidy_data)<-gsub("tBody", "TimeBody", names(tidy_data))
+names(tidy_data)<-gsub("mean", "Mean", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("std", "STD", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("angle", "Angle", names(tidy_data))
+names(tidy_data)<-gsub("gravity", "Gravity", names(tidy_data))
+
+
+# independent data set with the average of each variable for each activity
+# and each subject
+final_data <- tidy_data %>% group_by(Subject, Activity) %>% summarise_all(mean)
+write.table(final_data, "final_data.txt", row.name=FALSE)
+
